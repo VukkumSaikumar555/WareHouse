9cae4113325d7a62066b974540e9b9f16376714d
TL <TL@gmail.com>
2021-09-24 13:56:59 +0530

    WareHouseApp-1 Commited

---
+++ WareHouseApp-1/src/main/java/com/nt/WareHouseApp1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouseApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareHouseApp1Application.class, args);
+	}
+
+}
